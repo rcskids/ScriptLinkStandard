c6309c15f8a295591b80549dd6fb3ef1fd6be885
Scott Olson Jr <solson@rcskids.org>
2017-09-26 11:35:03 -0700

    Added exceptions

--- ScriptLinkStandard/Helpers/RemoveFieldObject.cs
+++ ScriptLinkStandard/Helpers/RemoveFieldObject.cs
@@ -10,8 +10,10 @@ namespace ScriptLinkStandard.Helpers
     {
         public static RowObject RemoveFieldObject(IRowObject rowObject, IFieldObject fieldObject)
         {
-            if (rowObject == null || fieldObject == null)
-                return (RowObject)rowObject;
+            if (rowObject == null)
+                throw new System.ArgumentException("Parameter cannot be null.", "rowObject");
+            if (fieldObject == null)
+                throw new System.ArgumentException("Parameter cannot be null.", "fieldObject");
             rowObject.Fields.Remove((FieldObject)fieldObject);
             return (RowObject)rowObject;
         }
@@ -19,10 +21,12 @@ namespace ScriptLinkStandard.Helpers
         public static RowObject RemoveFieldObject(IRowObject rowObject, string fieldNumber)
         {
             if (rowObject == null)
-                return (RowObject)rowObject;
+                throw new System.ArgumentException("Parameter cannot be null.", "rowObject");
+            if (fieldNumber == null || fieldNumber == "")
+                throw new System.ArgumentException("Parameter cannot be null or blank.", "fieldNumber");
             FieldObject fieldObject = rowObject.Fields.Find(f => f.FieldNumber == fieldNumber);
             if (fieldObject == null)
-                return (RowObject)rowObject;
+                throw new System.ArgumentException("The RowObject does not contain this FieldObject.", "fieldNumber");
             return RemoveFieldObject(rowObject, fieldObject);
         }
     }
