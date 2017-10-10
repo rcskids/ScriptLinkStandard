9a1e954053701da047f2a9eb1348d759d4e29df4
Scott Olson Jr <solson@rcskids.org>
2017-10-09 17:43:28 -0700

    Adds IsValidUrl Helper Method

---
+++ ScriptLinkStandard.Tests/HelpersTests/IsValidUrlTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScriptLinkStandard.Helpers;
+
+namespace ScriptLinkStandard.Tests.HelpersTests
+{
+    [TestClass]
+    public class IsValidUrlTests
+    {
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTP()
+        {
+            string strUrl = "http://everydaymatters.com";
+            Assert.IsTrue(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTPS()
+        {
+            string strUrl = "https://everydaymatters.com";
+            Assert.IsTrue(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTP_QueryString()
+        {
+            string strUrl = "http://everydaymatters.com/?e=123";
+            Assert.IsTrue(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTPS_QueryString()
+        {
+            string strUrl = "https://everydaymatters.com/?e=123";
+            Assert.IsTrue(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTP_IsFalse()
+        {
+            string strUrl = "everydaymatters.com";
+            Assert.IsFalse(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+
+        [TestMethod]
+        [TestCategory("Helpers-IsValidUrl")]
+        public void IsValidUrl_Absolute_HTTPS_IsFalse()
+        {
+            string strUrl = "every day matters.com:443";
+            Assert.IsFalse(ScriptLinkHelpers.IsValidUrl(strUrl));
+        }
+    }
+}
---
+++ ScriptLinkStandard/Helpers/IsValidUrl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptLinkStandard.Helpers
+{
+    public partial class ScriptLinkHelpers
+    {
+        public static bool IsValidUrl(string strUrl)
+        {
+            return Uri.IsWellFormedUriString(strUrl, UriKind.Absolute);
+        }
+    }
+}
