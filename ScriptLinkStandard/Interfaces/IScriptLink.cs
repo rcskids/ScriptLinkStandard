bed81990c2333158b35c9cae44e6508afee5b88c
Scott Olson Jr <solson@rcskids.org>
2017-10-18 10:59:40 -0700

    Updates IScriptLink to include OptionObject2015

--- ScriptLinkStandard/Interfaces/IScriptLink.cs
+++ ScriptLinkStandard/Interfaces/IScriptLink.cs
@@ -7,5 +7,6 @@ namespace ScriptLinkStandard.Interfaces
         string GetVersion();
         OptionObject ProcessScript(IOptionObject optionObject, string parameter);
         OptionObject2 ProcessScript(IOptionObject2 optionObject, string parameter);
+        OptionObject2015 ProcessScript(IOptionObject2015 optionObject, string parameter);
     }
 }
