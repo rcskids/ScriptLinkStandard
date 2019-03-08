82b05a24757fdf1393c8fd3902e45bb3919949f6
Information Technology <rcsadmin@rcskids.org>
2019-03-08 10:22:13 -0800

    Expanded SetFieldValue Unit Tests

--- ScriptLinkStandard.Tests/HelpersTests/SetFieldValueTests.cs
+++ ScriptLinkStandard.Tests/HelpersTests/SetFieldValueTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ScriptLinkStandard.Helpers;
 using ScriptLinkStandard.Objects;
+using System;
 
 namespace ScriptLinkStandard.Tests.HelpersTests
 {
@@ -28,6 +29,20 @@ namespace ScriptLinkStandard.Tests.HelpersTests
 
         [TestMethod]
         [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_FieldObject_Null()
+        {
+            var expected = new FieldObject();
+            FieldObject actual = null;
+
+            expected.FieldValue = "Test";
+            actual = (FieldObject)ScriptLinkHelpers.SetFieldValue(actual, "Test");
+
+            Assert.AreEqual(expected.FieldValue, actual.FieldValue);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
         public void SetFieldValue_RowObject_AreEqual()
         {
             string expected = "Test";
@@ -38,6 +53,25 @@ namespace ScriptLinkStandard.Tests.HelpersTests
             };
             var rowObject = new RowObject();
             rowObject.Fields.Add(fieldObject);
+
+            rowObject = (RowObject)ScriptLinkHelpers.SetFieldValue(rowObject, "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(rowObject, "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_RowObject_Null()
+        {
+            string expected = "Test";
+
+            var fieldObject = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            RowObject rowObject = null;
 
             rowObject = (RowObject)ScriptLinkHelpers.SetFieldValue(rowObject, "123", "Test");
             string actual = ScriptLinkHelpers.GetFieldValue(rowObject, "123");
@@ -207,6 +241,30 @@ namespace ScriptLinkStandard.Tests.HelpersTests
 
             formObject = (FormObject)ScriptLinkHelpers.SetFieldValue(formObject, "2||2", "123", "Test");
             string actual = ScriptLinkHelpers.GetFieldValue(formObject, "2||2", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_FormObject_Null()
+        {
+            string expected = "Test";
+
+            var fieldObject = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var rowObject = new RowObject
+            {
+                RowId = "1||1"
+            };
+            rowObject.Fields.Add(fieldObject);
+            FormObject formObject = null;
+
+            formObject = (FormObject)ScriptLinkHelpers.SetFieldValue(formObject, "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(formObject, "123");
 
             Assert.AreEqual(expected, actual);
         }
@@ -408,6 +466,46 @@ namespace ScriptLinkStandard.Tests.HelpersTests
 
         [TestMethod]
         [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_OptionObject_Null()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject("122");
+            var fieldObject02 = new FieldObject("123");
+            var fieldObject03 = new FieldObject("123");
+            var fieldObject04 = new FieldObject("123");
+            var rowObject01 = new RowObject("1||1");
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject("2||1");
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject("2||2");
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject("2||3");
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            OptionObject optionObject = null;
+
+            optionObject = (OptionObject)ScriptLinkHelpers.SetFieldValue(optionObject, "2", "2||3", "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "2", "2||3", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
         public void SetFieldValue_OptionObject2_OtherRow_MI_AreEqual()
         {
             string expected = "Test";
@@ -597,6 +695,281 @@ namespace ScriptLinkStandard.Tests.HelpersTests
 
             optionObject = (OptionObject2)ScriptLinkHelpers.SetFieldValue(optionObject, "1", "1||1", "122", "Test");
             string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "1", "1||1", "122");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_OptionObject2_Null()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject("122");
+            var fieldObject02 = new FieldObject("123");
+            var fieldObject03 = new FieldObject("123");
+            var fieldObject04 = new FieldObject("123");
+            var rowObject01 = new RowObject("1||1");
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject("2||1");
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject("2||2");
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject("2||3");
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            OptionObject2 optionObject = null;
+
+            optionObject = (OptionObject2)ScriptLinkHelpers.SetFieldValue(optionObject, "2", "2||3", "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "2", "2||3", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        public void SetFieldValue_OptionObject2015_OtherRow_MI_AreEqual()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject
+            {
+                FieldNumber = "122"
+            };
+            var fieldObject02 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject03 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject04 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var rowObject01 = new RowObject
+            {
+                RowId = "1||1"
+            };
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject
+            {
+                RowId = "2||1"
+            };
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject
+            {
+                RowId = "2||2"
+            };
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject
+            {
+                RowId = "2||3"
+            };
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            var optionObject = new OptionObject2015();
+            optionObject.Forms.Add(formObject01);
+            optionObject.Forms.Add(formObject02);
+
+            optionObject = (OptionObject2015)ScriptLinkHelpers.SetFieldValue(optionObject, "2", "2||3", "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "2", "2||3", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        public void SetFieldValue_OptionObject2015_CurrentRow_MI_AreEqual()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject
+            {
+                FieldNumber = "122"
+            };
+            var fieldObject02 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject03 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject04 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var rowObject01 = new RowObject
+            {
+                RowId = "1||1"
+            };
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject
+            {
+                RowId = "2||1"
+            };
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject
+            {
+                RowId = "2||2"
+            };
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject
+            {
+                RowId = "2||3"
+            };
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            var optionObject = new OptionObject2015();
+            optionObject.Forms.Add(formObject01);
+            optionObject.Forms.Add(formObject02);
+
+            optionObject = (OptionObject2015)ScriptLinkHelpers.SetFieldValue(optionObject, "2", "2||1", "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "2", "2||1", "123");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        public void SetFieldValue_OptionObject2015_CurrentRow_NotMI_AreEqual()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject
+            {
+                FieldNumber = "122"
+            };
+            var fieldObject02 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject03 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var fieldObject04 = new FieldObject
+            {
+                FieldNumber = "123"
+            };
+            var rowObject01 = new RowObject
+            {
+                RowId = "1||1"
+            };
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject
+            {
+                RowId = "2||1"
+            };
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject
+            {
+                RowId = "2||2"
+            };
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject
+            {
+                RowId = "2||3"
+            };
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            var optionObject = new OptionObject2015();
+            optionObject.Forms.Add(formObject01);
+            optionObject.Forms.Add(formObject02);
+
+            optionObject = (OptionObject2015)ScriptLinkHelpers.SetFieldValue(optionObject, "1", "1||1", "122", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "1", "1||1", "122");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [TestCategory("ScriptLinkHelpers")]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SetFieldValue_OptionObject2015_Null()
+        {
+            string expected = "Test";
+
+            var fieldObject01 = new FieldObject("122");
+            var fieldObject02 = new FieldObject("123");
+            var fieldObject03 = new FieldObject("123");
+            var fieldObject04 = new FieldObject("123");
+            var rowObject01 = new RowObject("1||1");
+            rowObject01.Fields.Add(fieldObject01);
+            var rowObject02 = new RowObject("2||1");
+            rowObject02.Fields.Add(fieldObject02);
+            var rowObject03 = new RowObject("2||2");
+            rowObject03.Fields.Add(fieldObject03);
+            var rowObject04 = new RowObject("2||3");
+            rowObject04.Fields.Add(fieldObject04);
+            var formObject01 = new FormObject
+            {
+                FormId = "1",
+                CurrentRow = rowObject01
+            };
+            var formObject02 = new FormObject
+            {
+                FormId = "2",
+                CurrentRow = rowObject02
+            };
+            formObject02.OtherRows.Add(rowObject03);
+            formObject02.OtherRows.Add(rowObject04);
+            formObject02.MultipleIteration = true;
+            OptionObject2015 optionObject = null;
+
+            optionObject = (OptionObject2015)ScriptLinkHelpers.SetFieldValue(optionObject, "2", "2||3", "123", "Test");
+            string actual = ScriptLinkHelpers.GetFieldValue(optionObject, "2", "2||3", "123");
 
             Assert.AreEqual(expected, actual);
         }
