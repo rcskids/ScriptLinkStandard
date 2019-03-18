9a2c7d182c243188cd9ac0df4d27d2feb741ee8a
Information Technology <rcsadmin@rcskids.org>
2019-03-18 09:59:43 -0700

    Adds ErrorCode

---
+++ ScriptLinkStandard.Tests/ObjectsTests/ErrorCodeTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScriptLinkStandard.Objects;
+
+namespace ScriptLinkStandard.Tests.ObjectsTests
+{
+    [TestClass]
+    public class ErrorCodeTests
+    {
+        [TestMethod]
+        public void ErrorCode_Error_Returns_1()
+        {
+            int expected = 1;
+            Assert.AreEqual(expected, ErrorCode.Error);
+        }
+
+        [TestMethod]
+        public void ErrorCode_OkCancel_Returns_2()
+        {
+            int expected = 2;
+            Assert.AreEqual(expected, ErrorCode.OkCancel);
+        }
+
+        [TestMethod]
+        public void ErrorCode_Info_Returns_3()
+        {
+            int expected = 3;
+            Assert.AreEqual(expected, ErrorCode.Info);
+        }
+
+        [TestMethod]
+        public void ErrorCode_YesNo_Returns_4()
+        {
+            int expected = 4;
+            Assert.AreEqual(expected, ErrorCode.YesNo);
+        }
+
+        [TestMethod]
+        public void ErrorCode_OpenUrl_Returns_5()
+        {
+            int expected = 5;
+            Assert.AreEqual(expected, ErrorCode.OpenUrl);
+        }
+
+        [TestMethod]
+        public void ErrorCode_OpenForm_Returns_6()
+        {
+            int expected = 6;
+            Assert.AreEqual(expected, ErrorCode.OpenForm);
+        }
+    }
+}
---
+++ ScriptLinkStandard/Objects/ErrorCode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScriptLinkStandard.Objects
+{
+    public static class ErrorCode
+    {
+        /// <summary>
+        /// Returns provided message with an Ok button. Stops script processing.
+        /// </summary>
+        public static int Error { get { return 1; } }
+        /// <summary>
+        /// Returns provided message with Ok and Cancel buttons. Stops script processing, if Cancel is selected.
+        /// </summary>
+        public static int OkCancel { get { return 2; } }
+        /// <summary>
+        /// Returns provided message with an Ok button.
+        /// </summary>
+        public static int Info { get { return 3; } }
+        /// <summary>
+        /// Returns provided message with Yes and No buttons. Stops script processing, if No is selected.
+        /// </summary>
+        public static int YesNo { get { return 4; } }
+        /// <summary>
+        /// Opens provided Url in default web browser.
+        /// </summary>
+        public static int OpenUrl { get { return 5; } }
+        /// <summary>
+        /// Returns a message with Ok and Cancel buttons. Opens specified form(s) if OK selected. Can only be used at Form Load and on Field events.
+        /// </summary>
+        public static int OpenForm { get { return 6; } }
+    }
+}
