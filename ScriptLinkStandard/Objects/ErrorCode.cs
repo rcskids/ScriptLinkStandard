536deba6795f4bd12152563c9eed542feeb155fb
Information Technology <rcsadmin@rcskids.org>
2019-03-18 10:05:43 -0700

    Refactor ErrorCode

--- ScriptLinkStandard/Objects/ErrorCode.cs
+++ ScriptLinkStandard/Objects/ErrorCode.cs
@@ -1,34 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ScriptLinkStandard.Objects
+﻿namespace ScriptLinkStandard.Objects
 {
     public static class ErrorCode
     {
         /// <summary>
         /// Returns provided message with an Ok button. Stops script processing.
         /// </summary>
-        public static int Error { get { return 1; } }
+        public const int Error = 1;
         /// <summary>
         /// Returns provided message with Ok and Cancel buttons. Stops script processing, if Cancel is selected.
         /// </summary>
-        public static int OkCancel { get { return 2; } }
+        public const int OkCancel = 2;
         /// <summary>
         /// Returns provided message with an Ok button.
         /// </summary>
-        public static int Info { get { return 3; } }
+        public const int Info = 3;
         /// <summary>
         /// Returns provided message with Yes and No buttons. Stops script processing, if No is selected.
         /// </summary>
-        public static int YesNo { get { return 4; } }
+        public const int YesNo = 4;
         /// <summary>
         /// Opens provided Url in default web browser.
         /// </summary>
-        public static int OpenUrl { get { return 5; } }
+        public const int OpenUrl = 5;
         /// <summary>
         /// Returns a message with Ok and Cancel buttons. Opens specified form(s) if OK selected. Can only be used at Form Load and on Field events.
         /// </summary>
-        public static int OpenForm { get { return 6; } }
+        public const int OpenForm = 6;
     }
 }
