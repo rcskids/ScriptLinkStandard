c48a1b45e52d5015466f5a526540b417919b2b5d
Information Technology <rcsadmin@rcskids.org>
2019-03-19 09:59:32 -0700

    Remove GetVersion requirement from IScriptLink

--- ScriptLinkStandard/Interfaces/IScriptLink.cs
+++ ScriptLinkStandard/Interfaces/IScriptLink.cs
@@ -4,7 +4,6 @@ namespace ScriptLinkStandard.Interfaces
 {
     public interface IScriptLink
     {
-        string GetVersion();
         OptionObject ProcessScript(IOptionObject optionObject, string parameter);
         OptionObject2 ProcessScript(IOptionObject2 optionObject, string parameter);
         OptionObject2015 ProcessScript(IOptionObject2015 optionObject, string parameter);
